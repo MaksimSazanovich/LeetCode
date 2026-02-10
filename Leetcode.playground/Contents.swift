5ca877abc18944cb4cac8dd1d5b8002f97e116d2
Unity_one_love <sazanovichmaksim@gmail.com>
2026-02-10 14:44:21 +0300

    136

--- Leetcode.playground/Contents.swift
+++ Leetcode.playground/Contents.swift
@@ -711,3 +711,19 @@ func findDisappearedNumbers(_ nums: [Int]) -> [Int] {
 
         return result
 }
+
+//MARK: 136. Single Number
+func singleNumber(_ nums: [Int]) -> Int {
+        var set = Set<Int>()
+
+        for num in nums {
+            if set.contains(num) {
+                set.remove(num)
+            } else {
+                set.insert(num)
+            }
+        }
+
+        return set.first ?? -1
+}
+
